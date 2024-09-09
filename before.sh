021fc05ae196401c9c7403c8a02d8b3a6321b0d1
Habibon <142244988+Habibon@users.noreply.github.com>
2024-09-09 17:35:43 +0300

    Update before.sh

--- before.sh
+++ before.sh
@@ -1,7 +1,7 @@
 uname -a
 sudo dnf update -y
-sudo sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-Linux-*
-sudo sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-Linux-*
+sudo sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-Stream-*
+sudo sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-Stream-*
 sudo dnf -y install wget tar nano
 wget https://gist.githubusercontent.com/cronfy/00e23e126d4fbf3fab37392e414f0e4c/raw/b74c1c300189bc8f3726dcda58940aa825bd09fb/dl-cloud-mail-ru.sh
 chmod +x dl-cloud-mail-ru.sh
